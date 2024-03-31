9691df84143382a4aee429f986f36c857e3b06e5
Guilherme Leroy <112895265+ggleroy@users.noreply.github.com>
2024-03-30 21:52:13 -0300

    Add Fib - recursividade

---
+++ recursividade/src/Fib.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Fib {
+    public static int fib(int n) {
+        if (n <= 1)
+            return n;
+        return fib(n - 1) + fib(n - 2);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.valueOf(scanner.nextLine());
+        System.out.println(fib(n));
+        scanner.close();
+    }
+}
